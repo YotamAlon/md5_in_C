1eb59e3caa7d33adedbc11bcd2852beb9b5c0532
Yotam Alon <yotamalon@gmail.com>
2015-12-30 13:33:59 -0800

    first commit

---
+++ md5.c
@@ -0,0 +1,9 @@
+#include <"md5.h">
+
+void public main(char **args)
+{
+	printf("Please enter the phrase to hash (limited to 100 characters):");
+	char *input;
+	gets(input, 100);
+	
+}
---
+++ md5.h
@@ -0,0 +1,9 @@
+#ifndef _md5_h
+#define _md5_h
+
+int WORDA = 0x01234567
+int WORDB = 0x89abcdef
+int WORDC = 0xfedcba98
+int WORDD = 0x76543210
+
+
